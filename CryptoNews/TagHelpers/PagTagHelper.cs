a9d36bf94778fe63fe2be8f88cfbf71386134f20
Victor <vitkow@tut.by>
2021-06-14 15:12:19 +0300

    correction of PagTagHelper, start CSS

--- CryptoNews/TagHelpers/PagTagHelper.cs
+++ CryptoNews/TagHelpers/PagTagHelper.cs
@@ -29,33 +29,36 @@ namespace CryptoNews.TagHelpers
         public override Task ProcessAsync(TagHelperContext cont, 
             TagHelperOutput output)
         {
-            output.TagName = "div";
-            output.Content.SetContent("<a href=\"~/\">Go home!</a>");
             
             var url = _urlHelper.GetUrlHelper(ViewContext);
-            var res = new TagBuilder("div");
+            var res = new TagBuilder("ul");
+            res.AddCssClass("pages-fastnav");
+
             for (int i=1; i<=PageInfo.CountPages; i++)
             {
-                var tag = new TagBuilder("a");
+                var tagA = new TagBuilder("a");                
                 string anchor = GetAnchorInnerHtml(i, PageInfo);
-                tag.Attributes["href"] = url.Action(PageAction, new { page=i, srcId = SourceId});
-                tag.InnerHtml.Append(anchor);
-                res.InnerHtml.AppendHtml(tag);
+                tagA.Attributes["href"] = url.Action(PageAction, 
+                    new { sourceId = SourceId, pageNumber = i });                
+                tagA.InnerHtml.Append(anchor);
+                var li = new TagBuilder("li");
+                li.InnerHtml.AppendHtml(tagA);
+                res.InnerHtml.AppendHtml(li);
             }
-            output.Content.AppendHtml(res.InnerHtml);
+            output.Content.AppendHtml(res);
             return base.ProcessAsync(cont, output);
         }
 
         public string GetAnchorInnerHtml(int i, PageInfo pi)
         {
             string anchor;
-            if (pi.CountPages <= 10)
+            if (pi.CountPages <= 1)
             {
                 anchor = i.ToString();
             }
             else
             {
-                if ((Math.Abs(i - pi.PageNumber) >= 2) && i != 0 && i != pi.CountPages)
+                if ((Math.Abs(i - pi.PageNumber) > 2) && i != 0 && i != pi.CountPages)
                     anchor = "...";
                 else anchor = i.ToString();
 
